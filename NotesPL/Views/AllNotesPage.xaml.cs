3fcfba8f80edb929a4c8b25fd22e87cd7fd52a05
ItzPaulz <paullarrea2004@gmail.com>
2024-12-22 22:17:50 -0500

    Se han puesto iconos, imagenes y pequeña descripcion mia,

--- NotesPL/Views/AllNotesPage.xaml.cs
+++ NotesPL/Views/AllNotesPage.xaml.cs
@@ -5,5 +5,11 @@ public partial class AllNotesPage : ContentPage
 	public AllNotesPage()
 	{
 		InitializeComponent();
+
 	}
+    private void ContentPage_NavigatedTo(object sender, NavigatedToEventArgs e)
+    {
+        PLnotesCollection.SelectedItem = null;
+    }
+
 }
